9b9d104f1d23886158e066b0c4b48e8f67e35097
Jaseem <jaseem@nizzcorp.com>
2025-04-02 12:41:06 +0530

    INK-18 rewritten connectdb file in ts

---
+++ backend/config/connectDb.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { MONGO_URI } from "../constance/env";
+
+/**
+ * @file connectDb 
+ * @author Jaseem
+ * @description Connect to MongoDB database using Mongoose.
+ * @returns {Promise<mongoose.Connection>} - A promise that resolves to the mongoose connection object.
+ */
+export const connectDB = async () => {
+  try {
+    const con = await mongoose.connect(MONGO_URI);
+    console.log(`DB Connected ${con.connection.host}`);
+    return con;
+  } catch (error:any) {
+    error.message && console.log(`Error When connecting to DB ${error.message}`);
+    throw new Error("Database connection failed");
+    // process.exit(1);
+  }
+};
