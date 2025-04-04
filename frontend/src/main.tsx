c43477e6cdee1c2a3d0a0fa35e9d0aa31e6e2971
haseenNizzcorp <haseen@nizzcorp.com>
2025-04-04 11:15:20 +0530

    installed react router dom and setup app.jsx and main.jsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './App.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        {/* <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/edit" element={<EditProfile />} /> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
